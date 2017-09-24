9ba9da212d65dc12721f12571442c075fe7f9a7b
tabatak <hamamoto.kazuto@gmail.com>
2017-09-24 18:06:00 +0900

    week1 problem01,02 passed!

---
+++ algorithms-on-strings/week1/problem01/trie.js
@@ -0,0 +1,54 @@
+const reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let lines = [];
+
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let trie = buildTrie(lines);
+  printTrie(trie);
+});
+
+
+
+function buildTrie(patterns){
+  let nodeIndex = 0;
+  let trie = new Map();
+  trie.set(nodeIndex.toString(), new Map());
+  nodeIndex++;
+  for(let i = 1; i < patterns.length; i++){
+    let currentNode = 0;
+    let currentPattern = patterns[i];
+
+    for (let j = 0; j < currentPattern.length; j++){
+      let currentSymbol = currentPattern[j];
+      if (trie.get(currentNode.toString()).has(currentSymbol)){
+        // currentNodeを参照先に移動する
+        currentNode = trie.get(currentNode.toString()).get(currentSymbol);
+        
+      }else{
+        // 新しいnodeの追加
+        trie.set(nodeIndex.toString(), new Map());
+        
+        // 新しいnodeへの参照の追加
+        trie.get(currentNode.toString()).set(currentSymbol, nodeIndex);
+
+        currentNode = nodeIndex;
+        nodeIndex++;
+      }
+    }
+  }
+  return trie;
+}
+
+function printTrie(trie){
+  for(let nodeIndex of trie.keys()){
+    let node = trie.get(nodeIndex);
+    for(let char of node.keys()){
+      console.log(`${nodeIndex}->${node.get(char)}:${char}`);
+    }      
+  }
+}
