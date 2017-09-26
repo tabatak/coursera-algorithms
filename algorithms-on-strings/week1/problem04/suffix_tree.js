1e29292fab6ad77d592d711ecf5caa4c453e5241
tabatak <hamamoto.kazuto@gmail.com>
2017-09-26 13:00:43 +0900

    nodeindex取得をクロージャにしただけ。。。

--- algorithms-on-strings/week1/problem04/suffix_tree.js
+++ algorithms-on-strings/week1/problem04/suffix_tree.js
@@ -1,48 +1,81 @@
 const reader = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  let lines = [];
-  
-  reader.on('line', (line) => {
+input: process.stdin,
+output: process.stdout
+});
+
+let getNodeIndex = (function(){
+    let idx = 0;
+    return () => {return idx++};
+})();
+
+let lines = [];
+
+reader.on('line', (line) => {
     lines.push(line);
-  });
-  reader.on('close', () => {
+});
+reader.on('close', () => {
     const text = lines[0];
-  
-    const trie = buildTrie(text);
 
+    const trie = buildTrie2(text);
+    // printTrie(trie, text);
     console.log(trie);
-  });
-  
-  function buildTrie(text){
-    // とりあえずtext から trieを作成してみる
-    let nodeIndex = 0;
-    let trie = new Map();
-    trie.set(nodeIndex, new Map());
-    nodeIndex++;
+});
 
+function buildTrie2(text){
+    let trie = new Map();
+    trie.set(getNodeIndex(), new Map());
     for(let textIndex = 0; textIndex < text.length; textIndex++){
         let currentPattern = text.slice(textIndex, text.length);
         let currentNode = 0;
-
         for (let j = 0; j < currentPattern.length; j++){
             let currentSymbol = currentPattern[j];
             if (trie.get(currentNode).has(currentPattern[j])){
                 currentNode = trie.get(currentNode).get(currentPattern[j]);
             }else{
+                const nodeIndex = getNodeIndex();
                 trie.set(nodeIndex, new Map());
                 trie.get(currentNode).set(currentPattern[j], nodeIndex);
                 currentNode = nodeIndex;
-                nodeIndex++;
             }
         }
     }
-    compressTrie(trie, 0, 0, new Map());
     return trie;
-  }
-  var called = 0;
-  function compressTrie(trie, textIndex, currentNode, suffixTree){
+}
+
+function buildTrie(text){
+    // とりあえずtext から trieを作成してみる
+    let trie = new Map();
+    trie.set(getNodeIndex(), new Map());
+
+    for(let textIndex = 0; textIndex < text.length; textIndex++){
+        let currentNode = 0;
+        for (let startIndex = textIndex; startIndex < text.length; startIndex++){
+            let key = Array.from(trie.get(currentNode).keys()).find((key) => text[key] === text[startIndex]);
+            if (key){
+                currentNode = trie.get(currentNode).get(key);
+            }else{
+                let nodeIndex = getNodeIndex();
+                trie.set(nodeIndex, new Map());
+                trie.get(currentNode).set(startIndex, nodeIndex);
+                currentNode = nodeIndex;
+            }
+        }
+    }
+    return trie;
+}
+
+function printTrie(trie, text){
+    for(let nodeIndex of trie.keys()){
+        let node = trie.get(nodeIndex);
+        for(let key of node.keys()){
+            console.log(`${nodeIndex}->${node.get(key)}:${text[key]}`);
+        }      
+    }
+}
+  
+
+var called = 0;
+function compressTrie(trie, textIndex, currentNode, suffixTree){
     console.log(called++);
 
     let length = 0;
@@ -61,5 +94,4 @@ const reader = require('readline').createInterface({
         }
     }
     console.log(suffixTree);
-  }
-  
+}
